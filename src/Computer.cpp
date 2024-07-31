ba67066f2e1b1e3dba9c85effec8b7ae00d97d95
Igor Mirsalikhov <mirsalikhov.igor@gmail.com>
2024-07-30 20:42:39 -0400

    Optimize load and execute functions

--- src/Computer.cpp
+++ src/Computer.cpp
@@ -5,8 +5,7 @@
 
 Computer::Computer(size_t mem_size) 
     : memory_size(mem_size), accum(0), instReg(0),
-      instCount(0), operCode(0),
-      operand(0) 
+      instCount(0), operCode(0), operand(0), status(OK) 
 {
     memory = new int[memory_size];
     init_mem();
@@ -15,7 +14,7 @@ Computer::Computer(size_t mem_size)
 Computer::Computer(const Computer& other) 
     : accum(other.accum), instReg(other.instReg),
       instCount(other.instCount), operCode(other.operCode),
-      operand(other.operand) 
+      operand(other.operand), status(other.status) 
 {
     memory_size = other.memory_size;
     memory = new int[memory_size];
@@ -69,8 +68,9 @@ void Computer::load(std::string file_name) {
 
     input.open(cwd);
     if (input.fail()) {
-        puts("\n*** Invalid file name                        ***");
-        puts("*** Simpletron execution abnormally terminated ***\n");
+        status = INVALID_FILE_NAME;
+        print_status();
+        
         input.close();
         exit(EXIT_FAILURE);
     }
@@ -108,73 +108,33 @@ void Computer::print_overflow() const {
 
 void Computer::execute() {
     /*
-    1. Fetch (read) the instrucion in (instCount) memory location and save it in instReg.
+    1. Fetch (read) the instruction in (instCount) memory location and save it in instReg.
     2. Split the instruction into operCode and operand
     3. Define the operation
 */
-    //fetch the contents of the (ic) memory location into the ir
-    bool exit = false;
-    while (!exit) {
-        
+    //fetch the contents of the (instCount) memory location into the instReg
+    bool exit_ = false;
+    while (!exit_) {
+        //read instruction
         instReg = memory[instCount];
+        if (check_instruction() == true) {
+            status = INVALID_INSTRUCTION;
+            print_status();
+            puts("*** Simpletron execution abnormally terminated ***\n");
+            exit(EXIT_FAILURE);
+        }
         operCode = instReg / 100;
         operand = instReg % 100;
-
-        switch(operCode) {
-        case READ:
-            putchar('?');
-            putchar(' ');
-            scanf("%d", &memory[operand]);
-            break;
-        case WRITE:
-            printf("%d\n", memory[operand]);
-            break;   
-        case LOAD:
-            accum = memory[operand];
-            break;   
-        case DIRECT_LOAD:
-            //get memory location
-            instCount++; //go to the next word
-            memory[operand] = memory[instCount];
-            break;   
-        case STORE:
-            memory[operand] = accum;
-            break;   
-        case ADD:
-            accum += memory[operand];
-            break;   
-        case SUBTRACT:
-            accum -= memory[operand];
-            break;   
-        case DIVIDE:
-            if (memory[operand] == 0) {
-                puts("*** Attemp to divide by zero                   ***");
-                puts("*** Simpletron execution abnormally terminated ***");
-                exit = true;
-            } 
-            else {
-                accum /= memory[operand];
-            }
-            break;   
-        case MULTIPLY:
-            accum *= memory[operand];
-            break;   
-        case HALT:
-            puts("*** Simpletron execution terminated ***");
-            exit = true;
-            break;   
-        case BRANCH:
-        case BRANCHNEG:
-        case BRANCHZERO:
-            break;
-        default:
-            puts("\n*** Invalid operation code                     ***");
-            puts("*** Simpletron execution abnormally terminated ***\n");
-            exit = true;
+        get_operation();
+        if (status != OK) {
+            print_status();
+            exit(EXIT_FAILURE);
         }
+
+        
         if (check_overflow()) {
             print_overflow();
-            exit = true;
+            exit_ = true;
         }
         if (operCode == BRANCH || (operCode == BRANCHNEG && accum < 0) || (operCode == BRANCHZERO && accum == 0)) {
             instCount = operand;
@@ -220,5 +180,89 @@ void Computer::get_cur_dir_path(char * cwd, size_t size) const {
     else {
         perror("getcwd() error");
         exit(EXIT_FAILURE);
+    }
+}
+
+bool Computer::check_instruction() const {
+    return (instReg < MIN_INSTR || instReg > MAX_INSTR);
+}
+
+enum ComputerStatus Computer::get_operation() {
+    switch(operCode) {
+    case READ:
+        putchar('?');
+        putchar(' ');
+        scanf("%d", &memory[operand]);
+        return OK;
+    case WRITE:
+        printf("%d\n", memory[operand]);
+        return OK;   
+    case LOAD:
+        accum = memory[operand];
+        return OK;
+    case DIRECT_LOAD:
+        //get memory location
+        instCount++; //go to the next word
+        memory[operand] = memory[instCount];
+        return OK;   
+    case STORE:
+        memory[operand] = accum;
+        return OK;   
+    case ADD:
+        accum += memory[operand];
+        return OK;   
+    case SUBTRACT:
+        accum -= memory[operand];
+        return OK;   
+    case DIVIDE:
+        if (memory[operand] == 0) {
+            puts("*** Simpletron execution abnormally terminated ***");
+            return DIVIDE_ZERO;
+        } 
+        else {
+            accum /= memory[operand];
+        }
+        return OK; 
+    case MULTIPLY:
+        accum *= memory[operand];
+        return OK;   
+    case HALT:
+        return END; 
+    case BRANCH:
+    case BRANCHNEG:
+    case BRANCHZERO:
+        return OK;
+    default:
+        status = INVALID_OPERATION;
+        print_status();
+        return ERROR;
+    }
+}
+
+
+void Computer::print_status() const {
+    switch (status) {
+    case INVALID_FILE_NAME:
+        puts("\n*** Invalid file name                        ***");
+        break;
+    case END:
+        puts("*** Simpletron execution terminated ***");
+        break;
+    case DIVIDE_ZERO:
+        puts("*** Attemp to divide by zero                   ***");
+        break;
+    case INVALID_INSTRUCTION:
+        puts("\n*** Invalid instruction                      ***");
+        break;
+    case INVALID_OPERATION:
+        puts("\n*** Invalid operation code                   ***");
+        break;
+    default:
+        puts("*** Unknown status                             ***");
+
+    }
+
+    if (status == INVALID_FILE_NAME || status == INVALID_INSTRUCTION || status == DIVIDE_ZERO || status == INVALID_OPERATION) {
+        puts("*** Simpletron execution abnormally terminated ***\n");
     }
 }
